f6b79ed570fb3a4e0ac17d2ae2617fc43ee2493a
Pi <pi@localhost>
2026-04-01 07:49:48 -0400

    [ie/threads] add Threads.net extractor
    (cherry picked from commit 7292c470a7d7ed1f53a7aa8aa904cb43e17f6f01)
    (cherry picked from commit f12a28e2172268909d3acdc647902935ea9aa7c6)

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -1956,6 +1956,10 @@ from .thisvid import (
     ThisVidMemberIE,
     ThisVidPlaylistIE,
 )
+from .threads import (
+    ThreadsIE,
+    ThreadsIOSIE,
+)
 from .threeqsdn import ThreeQSDNIE
 from .threespeak import (
     ThreeSpeakIE,
